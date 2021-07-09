c069844441162624f45fe26b35489ea131b9cae4
KISHAN GALGOTRA <kk845906@gmail.com>
2021-07-09 20:53:54 +0530

    KISHAN CHECK 5

--- app/src/main/java/com/example/docsscanning/SplashScreen.java
+++ app/src/main/java/com/example/docsscanning/SplashScreen.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
