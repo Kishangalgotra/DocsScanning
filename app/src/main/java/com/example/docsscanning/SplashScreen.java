11442ebe82d937c4908446fd0015fdc6bcd563ae
KISHAN GALGOTRA <kk845906@gmail.com>
2021-07-10 09:04:29 +0530

    KISHAN CHECK 5

--- app/src/main/java/com/example/docsscanning/SplashScreen.java
+++ app/src/main/java/com/example/docsscanning/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.docsscanning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
@@ -11,6 +13,8 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Kishan","sdsadsa");
+        //CountDownTimer countDownTimer =thic
+        Intent intent =  new Intent(this,DashBoard.class);
+        startActivity(intent);
     }
 }
